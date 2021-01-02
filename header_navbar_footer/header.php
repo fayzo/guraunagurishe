c6c27b25e4a9f737800f39b10841253a2ca5251d
fayzo <shemafaysal@gmail.com>
2021-01-02 18:53:32 +0200

    first commit

---
+++ header_navbar_footer/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>OneTech</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="OneTech shop project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL_LINK ;?>plugins/bootstrap4/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL_LINK ;?>plugins/fontawesome-free-5.0.1/css/fontawesome-all.css">
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL_LINK ;?>plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL_LINK ;?>plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL_LINK ;?>plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL_LINK ;?>plugins/slick-1.8.0/slick.css">
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL_LINK ;?>dist/styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL_LINK ;?>dist/styles/responsive.css">
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL_LINK ;?>dist/css/cart_items.css">
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL_LINK ;?>dist/css/blog.css">
+
+</head>
+
+<body>
